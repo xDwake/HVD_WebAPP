7e160e11aaed6e03001b8e534778835b86091ca4
Gabriel Silva <73436907+xDwake@users.noreply.github.com>
2025-10-31 20:09:44 +0000

    Update UI.py

--- UI.py
+++ UI.py
@@ -61,7 +61,7 @@ LABEL_COLS = ['Self-direction', 'Stimulation', 'Hedonism', 'Achievement', 'Power
 # CRITICAL FIX: Point to the deployment-ready folder
 SAVED_MODEL_PATH = "xdwake/HVD_distilbert"
 METRICS_FILE_PATH = "./thesis_logs/all_experiment_metrics.csv"
-HYPERPARAMS_FILE_PATH = "./final_model/distilbert_focallos/hyperparameters.json"
+HYPERPARAMS_FILE_PATH = "./hyperparameters.json"
 
 COHERE_MODEL_NAME = "command-r-plus-08-2024"
 
@@ -734,3 +734,4 @@ with tab1:
 
 with tab2:
     model_page()
+
